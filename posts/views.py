9ce5c7a49146b04f75e225189d5dc8c69495a67c
wisamosman <viesam_1987@hotmail.com>
2023-06-23 00:52:47 +0200

    add edit post , delete post

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Post
 from .forms import PostForm
 
@@ -20,9 +20,34 @@ def new_post(request):
         form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            return redirect('/blog/')
 
     else:
         form = PostForm()    
     
 
     return render(request,'posts/new.html',{'form':form})
+
+
+
+def edit_post(request,post_id):
+    data = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES,instance=data)
+        if form.is_valid():
+            form.save()
+            return redirect('/blog/')
+
+    else:
+        form = PostForm(instance=data)    
+    
+
+    return render(request,'posts/edit.html',{'form':form})
+
+
+
+
+def delete_post(request,post_id):
+    data = Post.objects.get(id=post_id)
+    data.delete()
+    return redirect('/blog/')
