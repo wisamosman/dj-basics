a3a3b9d9d7be996e96c3c9b285931811adb630e8
wisamosman <viesam_1987@hotmail.com>
2023-06-22 22:25:22 +0200

    customize admin for posts

--- posts/admin.py
+++ posts/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Author
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish_date']
+    list_filter = ['author', 'publish_date', 'tags']
+    search_fields = ['title', 'content']
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
