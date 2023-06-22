2031389b49f915700e452a390495513e8995b84b
wisamosman <viesam_1987@hotmail.com>
2023-06-22 22:20:07 +0200

    add tags librerey

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='authors')
 
     def __str__(self):
         return self.name
@@ -15,6 +17,8 @@ class Post(models.Model):
     publish_date = models.DateTimeField()
     content = models.TextField(max_length=15000)
     author = models.ForeignKey(Author,related_name='post_author',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
